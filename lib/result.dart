4d09ed9edffec5fa4e252fa546c5f2815e9ecdb9
Pedro Barata <pedrofelipebarata@gmail.com>
2019-07-14 17:12:37 -0300

    feat(51): Getters and "else-if"

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int totalScore;
+
+  Result(this.totalScore);
+
+  String get resultPhrase {
+    String resultText;
+    if (totalScore <= 5) {
+      resultText = "You are innocent!";
+    } else if (totalScore <= 8) {
+      resultText = "You are awesome!";
+    } else if (totalScore <= 8) {
+      resultText = "You are so strange...";
+    } else {
+      resultText = "You are so bad!";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You did it!"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
