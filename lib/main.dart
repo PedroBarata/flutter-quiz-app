37f64b6c35d56b3740bb01ba956a7945545a9bf0
Pedro Barata <pedrofelipebarata@gmail.com>
2019-07-14 15:23:30 -0300

    feat(48): Outputting Widgets Conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  var _questions = [
+  final _questions = const [
     {
       "questionText": "What\'s your favorite animal?",
       "answers": ["Dog", "Rabbit", "Snake", "Cat"]
@@ -37,6 +37,11 @@ class _MyAppState extends State<MyApp> {
 
   void _answerQuestion() {
     setState(() => _questionIndex++);
+    if (_questionIndex < _questions.length) {
+      print("One more question!");
+    } else {
+      print("End of questions");
+    }
     print(_questionIndex);
   }
 
@@ -44,21 +49,26 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My first App'),
-          ),
-          body: Column(
-            children: <Widget>[
-              Question(
-                questionText: _questions[_questionIndex]["questionText"],
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(
+                    questionText: _questions[_questionIndex]["questionText"],
+                  ),
+                  ...(_questions[_questionIndex]["answers"] as List<String>)
+                      .map((answer) {
+                    return Answer(
+                        selectHandler: _answerQuestion, answerText: answer);
+                  }),
+                ],
+              )
+            : Center(
+                child: Text("You did it!"),
               ),
-              ...(_questions[_questionIndex]["answers"] as List<String>)
-                  .map((answer) {
-                return Answer(
-                    selectHandler: _answerQuestion, answerText: answer);
-              }),
-            ],
-          )),
+      ),
     );
   }
 }
